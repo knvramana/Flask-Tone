3e45d8d9266958ab6ccc353538e187d71fad2fb3
RAMANA K N V <51579733+knvramana@users.noreply.github.com>
2022-07-07 16:20:07 +0530

    Update flaskBackend.py
    With Algorithm

--- flaskBackend.py
+++ flaskBackend.py
@@ -1,8 +1,20 @@
 import os
 from flask import Flask, request, send_from_directory, send_file
 from flask_cors import CORS, cross_origin
-import string
-import random
+from subprocess import run, PIPE
+from flask import Flask, render_template, request
+from logger import logging
+import wave
+import pyttsx3
+
+import librosa
+import numpy as np
+
+chunk = 1024  # Record in chunks of 1024 samples
+channels = 2
+fs = 44100  # Record at 44100 samples per second
+seconds = 8
+frames = []
 
 app = Flask(__name__)
 cors = CORS(app)
@@ -25,16 +37,6 @@ def dev():
         return send_from_directory(f"{os.getcwd()}/app/dist/app", "index.html")
 
 
-# @app.route("/", methods=['GET'])
-# def dev():
-#     print("inside Main ")
-#     # if request.args:
-#     #     key = list(request.args)[0]
-#     #     return send_from_directory(f"{os.getcwd()}/dist/demo", key)
-#     # else:
-#     # print("******", f"{os.getcwd()}dist/demo")
-#     return send_from_directory(f"{os.getcwd()}/app/dist/demo", "index.html")
-
 @app.route('/save', methods=['POST'])   
 def save():
     print("** Insie Save Method ** ")
@@ -44,13 +46,36 @@ def save():
     print("\n\n")
 
     # res = ''.join(random.choices(string.ascii_uppercase + string.digits, k =7))
-    res = 'output'
+    res = 'output2'
     fl_name = res + '.wav'
     if file:
         file.save(os.path.join(app.config['UPLOAD_FOLDER'], fl_name))
+        print(fl_name)
+        
+    audio_data = './Downloads/output2.wav'	
+    x , sr = librosa.load(audio_data)
+    librosa.load(audio_data, sr=44100)
+    result= np.percentile(x,95)
+    print(" ")
+    print("Tone value is=",result )
+    print(" ")
+    engine = pyttsx3.init() 
+  
+# testing 
+    if result < 0.06 :
+            print ("This voice tone = Whisper")
+            engine.say("You spoke in Whisper")
+            engine.runAndWait() 
+    elif result >0.06 and result< 0.08:
+            print ("This voice tone = Soft")
+            engine.say("You spoke in Soft Tone")
+            engine.runAndWait() 
+    elif result > 0.08:
+            print ("This voice tone = Loud")
+            engine.say("You spoke in Loud Tone")
+            engine.runAndWait()			 
+
     return "success"
-
-
 
 
 # ======================================
